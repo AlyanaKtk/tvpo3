0f25efd41142ffe0d78173d0bbe1cb779c96bb53
AlyanaKtk <varkatkantas413@gmail.com>
2023-10-10 20:23:31 +0300

    первый итоговый TDD-тест и соответствующая фукция

--- src/main/java/matrixcalc/matrix.java
+++ src/main/java/matrixcalc/matrix.java
@@ -1,4 +1,18 @@
 package matrixcalc;
 
 public class matrix {
+    public int[][] add(int[][] A, int[][] B)
+    {
+        if (A.length != B.length || A[0].length != B[0].length)
+        {
+            System.out.println("Матрицы неравны");
+            return null;
+        }
+        int C[][] = new int[A.length][A.length];
+        for (int i = 0; i < A.length; i++)
+            for (int j = 0; j < A.length; j++)
+                C[i][j] = A[i][j] + B[i][j];
+        return C;
+
+    }
 }
--- src/test/java/matrixcalc/testsummatrix.java
+++ src/test/java/matrixcalc/testsummatrix.java
@@ -3,18 +3,20 @@ package matrixcalc;
 import junit.framework.TestCase;
 import org.junit.Test;
 
-public class matrixSumTest extends TestCase {
+public class testsummatrix extends TestCase {
 
     @Test
-    public void matrixSum()
+    public void testMatrixSum()
     {
         int[][] matrixA = {{1, 2, 3}, {4,7,9}, {8, 11, 0}};
         int[][] matrixB = {{8, 13, 3}, {5,0,9}, {14, 1, 8}};
         matrix mtrx = new matrix();
         int[][] matrixCAnswer = {{9, 15, 6}, {9, 7, 18}, {22, 12, 8}};
         int[][] matrixC = mtrx.add(matrixA, matrixB);
-        assertEquals(matrixC, matrixCAnswer);
-        
+        for (int i = 0; i < matrixA.length; i++)
+            for (int j = 0; j < matrixA.length; j++)
+                assertEquals(matrixC[i][j], matrixCAnswer[i][j]);
+
 
     }
 
